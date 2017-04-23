40fb82348cb14ed5a90215d4fb5b45751d501354
Christopher Davenport <Chris@ChristopherDavenport.tech>
2017-04-23 16:05:42 -0400

    Integrate Process Components

--- app/Main.hs
+++ app/Main.hs
@@ -9,6 +9,9 @@ import Lib
 import Options.Applicative
 import System.Environment
 import Text.Read
+import System.Directory
+import System.Exit
+import System.Process
 
 import qualified Data.Text as Text
 import qualified Options.Applicative
@@ -37,10 +40,27 @@ opts =
     (fullDesc <> progDesc "Check Ansible Playbook" <>
      header "ansiblecheck - a testing framework for ansible")
 
+--main :: IO ()
+--main = do
+--  options <- execParser opts
+--  print (writeOS $ fromOptions options)
+
 main :: IO ()
 main = do
-  options <- execParser opts
-  print (writeOS $ fromOptions options)
+  fp <- System.Directory.getCurrentDirectory
+  runXenial fp
+
+
+runXenial :: FilePath -> IO()
+runXenial fp = do
+  _ <- pullDocker (Ubuntu Xenial)
+  _ <- runDocker fp (Ubuntu Xenial)
+  _ <- syntaxCheckDocker (Ubuntu Xenial)
+  _ <- testDocker (Ubuntu Xenial)
+  _ <- stopDocker (Ubuntu Xenial)
+  _ <- removeDocker (Ubuntu Xenial)
+  return ()
+
 
 writeOS :: Maybe OperatingSystem -> String
 writeOS mOS =
@@ -75,6 +95,57 @@ data ServiceManager
   = SystemD
   | Init
   deriving (Eq, Show, Read, Ord)
+
+
+pullDocker :: OperatingSystem -> IO ExitCode
+pullDocker _ =
+  System.Process.system ("docker pull ansiblecheck/ansiblecheck:" ++ "ubuntu" ++ "-" ++ "xenial")
+
+runDocker :: FilePath -> OperatingSystem -> IO ExitCode
+runDocker fp _ =
+  System.Process.system (
+  "docker run --detach" ++
+  " " ++
+  "--name=" ++ "ansiblecheck-ubuntu-xenial" ++
+  " " ++
+  "--volume=" ++ show fp ++ ":/etc/ansible/roles/role_under_test:ro" ++
+  " " ++
+  "--volume=/sys/fs/cgroup:/sys/fs/cgroup:ro" ++
+  " " ++
+  "ansiblecheck/ansiblecheck:" ++ "ubuntu" ++ "-" ++ "xenial" ++
+  " " ++
+  "/lib/systemd/systemd"
+  )
+
+syntaxCheckDocker :: OperatingSystem -> IO ExitCode
+syntaxCheckDocker _ =
+  System.Process.system (
+  "docker exec --tty"
+  ++ " "
+  ++ "ansiblecheck-ubuntu-xenial"
+  ++ " "
+  ++ "env TERM=xterm"
+  ++ " "
+  ++ "ansible-playbook /etc/ansible/roles/role_under_test/tests/test.yml --syntax-check"
+  )
+
+testDocker :: OperatingSystem -> IO ExitCode
+testDocker _ =
+  System.Process.system (
+  "docker exec"
+  ++ " "
+  ++ "ansiblecheck-ubuntu-xenial"
+  ++ " "
+  ++ "ansible-playbook /etc/ansible/roles/role_under_test/tests/test.yml"
+  )
+
+stopDocker :: OperatingSystem -> IO ExitCode
+stopDocker _ =
+  System.Process.system "docker stop ansiblecheck-ubuntu-xenial"
+
+removeDocker :: OperatingSystem -> IO ExitCode
+removeDocker _ =
+  System.Process.system "docker rm ansiblecheck-ubuntu-xenial"
 
 osServiceManager :: OperatingSystem -> ServiceManager
 osServiceManager (Ubuntu Yakkety) = Init
